3c36cf19a8cfc89ec3f92d873df040131ada4d8d
bobiko23 <55994876+bobiko23@users.noreply.github.com>
2021-11-30 14:01:24 +0200

    Test added

---
+++ EventsCalendar/src/edu/school/calendar/Test.java
@@ -0,0 +1,25 @@
+package edu.school.calendar;
+
+public class Test 
+{
+    public static void main(String ards[])
+    {
+        Time time = new Time(5, 45, 59);
+
+        time.increaseSecond();
+
+        System.out.println(time);
+
+        Time time2 = new Time(5, 59, 59);
+
+        time2.increaseSecond();
+
+        System.out.println(time2);
+
+        Time time3 = new Time(23, 59, 59);
+        
+        time3.increaseSecond();
+
+        System.out.println(time3);
+    }
+}
--- EventsCalendar/src/edu/school/calendar/Time.java
+++ EventsCalendar/src/edu/school/calendar/Time.java
@@ -1,5 +1,103 @@
 package edu.school.calendar;
 
-public class Time {
+public class Time 
+{
+    private int hour;
+    private int second;
+    private int minute;
 
+    public void setHour(int hourValue)
+    {
+        if(hourValue >= 0 && hourValue < 24)
+        {
+            hour = hourValue;
+        }
+    }
+
+    public void setMinnute(int minuteValue)
+    {
+        if(minuteValue >= 0 && minuteValue < 60)
+        {
+            minute = minuteValue;
+        }
+    }
+
+    public void setSecond(int secondValue)
+    {
+        if(secondValue >= 0 && secondValue < 60)
+        {
+            second = secondValue;
+        }
+    }
+
+    public int getHour()
+    {
+        return hour;
+    }
+
+    public int getMinute()
+    {
+        return minute;
+    }
+
+    public int getSecond()
+    {
+        return second;
+    }
+
+    public Time(int hour, int minute, int second)
+    {
+        setHour(hour);
+        setMinnute(minute);
+        setSecond(second);
+    }
+
+    public Time()
+    {
+        setHour(0);
+        setMinnute(0);
+        setSecond(0);
+    }
+
+    String toString()
+    {
+        String res = "";
+
+        if(getHour() < 10)
+        {
+            res += "0" + getHour() + ":";
+        }
+        if(getMinute() < 10)
+        {
+            res += "0" + getMinute() + ":";
+        }
+        if(getSecond() < 10)
+        {
+            res += "0" + getSecond();
+        }
+
+        return res;
+    }
+
+    public void increaseSecond()
+    {
+        second ++;
+
+        if(second > 60)
+        {
+            second = 0;
+            minute ++;
+        }
+
+        if(minute > 60)
+        {
+            minute = 0;
+            hour ++;
+        }
+
+        if(hour > 24)
+        {
+            hour = 0;
+        }
+    }
 }
